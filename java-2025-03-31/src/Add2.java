aab47078fcbb042248359334ed9b27261a4d4a31
jjangjay <jjangjay1@gmail.com>
2025-03-31 14:44:40 +0900

    scanner

---
+++ java-2025-03-31/src/Add2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Add2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num1, num2;
+
+        System.out.print("숫자를 입력하시오: ");
+        num1 = sc.nextInt();
+        System.out.print("숫자를 입력하시오: ");
+        num2 = sc.nextInt();
+
+        System.out.println(num1 + num2);
+    }
+}

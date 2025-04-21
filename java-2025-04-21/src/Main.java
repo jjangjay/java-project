576526b9a1a087f1934de040e55582850b67fe60
jjangjay <jjangjay1@gmail.com>
2025-04-21 14:02:33 +0900

    수의 범위 확인 프로그램

---
+++ java-2025-04-21/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        if (1 <= num && num <= 100) {
+            System.out.println("1에서 100 사이의 값입니다.");
+        } else {
+            System.out.println("1에서 100 사이의 값이 아닙니다.");
+        }
+    }
+}
